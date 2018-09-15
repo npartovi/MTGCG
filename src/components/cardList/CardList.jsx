9e6be8c5b54f6f82fd212ecc6e0dca096e6ee540
yujie zhu <yujiezhu@yujies-WB-MacBook-Pro.local>
2018-09-15 15:46:23 -0700

    fixed merge conflicts

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -5,11 +5,6 @@ import Card from './Card'
 class CardList extends Component {
     
     render() {
-<<<<<<< HEAD
-        console.log(this.props.cards);
-
-        
-=======
         
         const {cards} = this.props
 
@@ -18,7 +13,6 @@ class CardList extends Component {
         ))
      
 
->>>>>>> 48bf1421ce4b731b1acada1bb3c80cb74359051c
         return (
             <div>{cardListView}</div>
         );
