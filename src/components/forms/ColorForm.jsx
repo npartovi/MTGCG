7419be9d78a2784cef2452c4be77d4dc105df9a1
npartovi <npartovi@gmail.com>
2018-09-15 12:31:25 -0700

    starting on form

---
+++ src/components/forms/ColorForm.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+
+class ColorForm extends Component {
+
+    render(){
+        return(
+            <h1>This is a form</h1>
+        )
+    }
+}
+
+export default ColorForm
