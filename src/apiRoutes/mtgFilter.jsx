a32ecf3213b660c6640923a64777525bb1ff0eff
npartovi <npartovi@gmail.com>
2018-09-15 13:10:30 -0700

    got color selection working

---
+++ src/apiRoutes/mtgFilter.jsx
@@ -0,0 +1,8 @@
+export const getCards = (colors) => {
+
+    let colorString = colors.join(",");
+    mtg.card.all({ supertypes: 'legendary', types: 'creature', colors: colorString })
+        .on('data', function (card) {
+            console.log(card.name)
+        });
+}
