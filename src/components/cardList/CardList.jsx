9589fd7dddcc0a089a3c52c03c320736ed0d20fb
Nick Welch <nick.welch@buildingconnected.com>
2018-09-21 20:45:40 -0700

    Prettify everything

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,26 +1,29 @@
-import {connect} from "react-redux" 
-import React, {Component} from "react"
+import { connect } from 'react-redux'
+import React, { Component } from 'react'
 import Card from './Card'
 
-
 class CardList extends Component {
-   
-    render() {
-        const {cards} = this.props;
-        const cardListView = cards.map((card,idx) => (
-            <Card key={idx} card={card} index={idx}/>
-        ))
-        return (
-            <div className="card-list-container" onClick={this.changeCard}>
-                {cardListView}
-            </div>
-        );
-    }
+  render() {
+    const { cards } = this.props
+    const cardListView = cards.map((card, idx) => (
+      <Card key={idx} card={card} index={idx} />
+    ))
+    return (
+      <div
+        className="card-list-container"
+        onClick={this.changeCard}
+      >
+        {cardListView}
+      </div>
+    )
+  }
 }
 
-
 const mapStateToProps = state => ({
-    cards: state.cards
+  cards: state.cards
 })
 
-export default connect(mapStateToProps, null)(CardList);
+export default connect(
+  mapStateToProps,
+  null
+)(CardList)
