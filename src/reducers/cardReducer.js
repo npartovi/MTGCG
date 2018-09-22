21467343b9b4995c8c591e06ff26199507c76c3c
Nick Welch <nick.welch@buildingconnected.com>
2018-09-21 20:39:18 -0700

    Move mtgFilter into separate folder

--- src/reducers/cardReducer.js
+++ src/reducers/cardReducer.js
@@ -1,20 +1,23 @@
-import { GET_ALL_CARDS, GET_CARD } from '../actions/types';
-const initialState = [];
+import {
+  GET_ALL_CARDS,
+  GET_CARD
+} from '../actions/filter/types'
+const initialState = []
 
 const cardReducer = (state = initialState, action) => {
-  Object.freeze(state);
-  let newState;
+  Object.freeze(state)
+  let newState
   switch (action.type) {
     case GET_ALL_CARDS:
-      newState = action.payload;
-      return newState;
+      newState = action.payload
+      return newState
     case GET_CARD:
-      newState = state.slice();
-      newState[action.payload.idx] = action.payload.card;
-      return newState;
+      newState = state.slice()
+      newState[action.payload.idx] = action.payload.card
+      return newState
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default cardReducer;
+export default cardReducer
--- src/reducers/colorReducer.js
+++ src/reducers/colorReducer.js
@@ -1,16 +1,16 @@
-import { GET_ALL_CARDS } from '../actions/types';
-const initialState = '';
+import { GET_ALL_CARDS } from '../actions/filter/types'
+const initialState = ''
 
 const colorReducer = (state = initialState, action) => {
-  Object.freeze(state);
-  let newState;
+  Object.freeze(state)
+  let newState
   switch (action.type) {
     case GET_ALL_CARDS:
-      newState = action.colors;
-      return newState;
+      newState = action.colors
+      return newState
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default colorReducer;
+export default colorReducer
