30e63bb762fcdf916c368b4182df8eb4e1d491f2
Nick Welch <nick.welch@buildingconnected.com>
2018-09-21 20:37:29 -0700

    Add loading reducer file

---
+++ src/reducers/loadingReducer.js
@@ -0,0 +1,23 @@
+import {
+  START_LOADING,
+  LOADING_SUCCESS
+} from '../actions/types'
+
+const initialState = {
+  global: false
+}
+
+const loadingReducer = (state = initialState, action) => {
+  Object.freeze(state)
+  let newState
+  switch (action.type) {
+    case START_LOADING:
+      return { ...newState, global: true }
+    case LOADING_SUCCESS:
+      return { ...newState, global: false }
+    default:
+      return state
+  }
+}
+
+export default colorReducer
